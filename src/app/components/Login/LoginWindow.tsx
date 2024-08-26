b6e667d5d60d75ac582223d10338e97d10ff9398
Otto Suhett <ottosuhett@gmail.com>
2024-08-26 19:59:13 +0200

    Adicao da fonte Orbitron e da mensagem no container esquerdo e tb seus estilos

--- src/app/components/Login/LoginWindow.tsx
+++ src/app/components/Login/LoginWindow.tsx
@@ -8,7 +8,9 @@ export interface ILoginWindowProps {
 export default function LoginWindow (props: ILoginWindowProps) {
   return (
     <Container fluid className={styles.mainContainer}>
-      <div className={styles.leftContainer}></div>
+      <div className={styles.leftContainer}>
+        <p className={styles.leftDrescriptionMsg}>Store your <span className={styles.highlightMsg}>favorite</span> media</p>
+      </div>
       <div className={styles.rightContainer}></div>
     </Container>
   );
