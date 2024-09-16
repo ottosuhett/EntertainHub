91a44e0db0cb66d8cb5348203565c70f14ea61a5
Otto Suhett <ottosuhett@gmail.com>
2024-09-16 23:44:56 +0200

    Atualizacao do estilo da lista e aplicacao de classe condicional

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -2,6 +2,7 @@ import React,{useContext, useEffect, useState} from 'react';
 import { MainContext } from "@/app/context/MainContext";
 import {Form, Button,Container,Row,Col,Card} from 'react-bootstrap';
 import { getGameList } from "@/app/functions/genericFunctions";
+import ButtonComp from '../Btns/ButtonComp';
 import styles from "./GameList.module.scss"
 export interface IGameListProps {
 }
@@ -26,12 +27,16 @@ export default function GameList (props: IGameListProps) {
     );
 
     const listToRender = searchedGame.length > 0 ? filteredGameList : gameList
+
     
   return (
-    <div className={styles.gameList}>
+    <div className={listToRender.length <9 ? styles.shortList:styles.gameList}>
         {gameList.length > 0 ? (
           listToRender.map((game) => (
-              <Card className={styles.cardGame}>
+              <Card 
+              className={styles.cardGame}
+              key={game.id}
+              >
                 <Card.Img 
                 variant="top" 
                 src={game.background_image} 
@@ -42,7 +47,11 @@ export default function GameList (props: IGameListProps) {
                     <Card.Text>
                     {game.id}
                     </Card.Text>
-                    <Button variant="primary">Show More</Button>
+                    <div className={styles.btnContainer}>
+                        <Button variant="primary">Show more</Button>
+                        <Button variant="primary">Add to list</Button>
+                    </div>
+                    
                 </Card.Body>
             </Card>
           ))
