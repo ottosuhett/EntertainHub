85555d8c8f4dae35f58b42499a4d8711eb13c899
Otto Suhett <ottosuhett@gmail.com>
2024-10-11 20:21:20 +0200

    Alteracoes feitas para atualizar a tab ativa da navbar quando atualiza a pagina

--- src/app/myList/page.tsx
+++ src/app/myList/page.tsx
@@ -1,13 +1,19 @@
 "use client"
-import React from 'react';
+import React,{useEffect, useContext} from 'react';
 import styles from "./page.module.scss"
 import MainNavBar from '@/components/MainNavBar/MainNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyUserList from '@/components/MyList/MyUserList';
+import { MainContext } from '../context/MainContext';
 export interface IMyListProps {
 }
 
 export default function MyListPage (props: IMyListProps) {
+  const {setSelectedNavBar} = useContext(MainContext)
+  useEffect(()=>{
+    setSelectedNavBar("MyList")
+    },[])
+  
   return (
     <div className={styles.mainContainer}>
       <MainNavBar />
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,20 @@
 "use client"
-import React from 'react';
+import React, { useEffect,useContext } from 'react';
 import SearchContent from '@/components/SearchContent/SearchContent';
 import styles from "./page.module.scss"
 import MainNavBar from '@/components/MainNavBar/MainNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { MainContext } from '../context/MainContext';
 
 export interface ISearchProps {
 }
-
+ 
 export default function Search (props: ISearchProps) {
+  const {setSelectedNavBar} = useContext(MainContext)
+
+  useEffect(()=>{
+    setSelectedNavBar("Search")
+   },[])
   return (
     <div className={styles.mainContainer}>
       <MainNavBar />
